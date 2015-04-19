feaeec713352ed6a616e72614e933c9c96c835fe
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2015-04-19 15:12:01 +0200

    Initial layout for other pages than landing

--- themes/CherryTea.org/prepare.php
+++ themes/CherryTea.org/prepare.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * @package		ClevereStyle CMS
- * @subpackage	CleverStyle theme
- * @author		Nazar Mokrynskyi <nazar@mokrynskyi.com>
- * @copyright	Copyright (c) 2013-2015, Nazar Mokrynskyi
- * @license		MIT License, see license.txt
+ * @package   CherryTea.org
+ * @category  themes
+ * @author    Nazar Mokrynskyi <nazar@mokrynskyi.com>
+ * @copyright Copyright (c) 2015, Nazar Mokrynskyi
+ * @license   MIT License, see license.txt
  */
-namespace	cs;
+namespace cs;
+$Page = Page::instance();
 /**
  * @var _SERVER $_SERVER
  */
 if (preg_match('/msie|trident/i', $_SERVER->user_agent)) {
-	Page::instance()->Head	.= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
+	$Page->Head .= '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">';
 }
+$Page->level['Content'] = 3;
